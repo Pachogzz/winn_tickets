736e652071c0f808d6ac44f79572ed774ca622b8
Andrés Venegas <andresvenegas@MacBook-Pro.local>
2020-07-01 21:25:48 -0500

    Se agrego input por cada boleto en pedidos

--- inc/boletos.php
+++ inc/boletos.php
@@ -259,17 +259,17 @@ add_filter( 'woocommerce_get_item_data', 'plugin_republic_get_item_data', 10, 2 
 /**
  * Add custom cart item data to emails
  */
-function plugin_republic_order_item_name( $product_name, $item ) {
-    if( isset( $item['pr_field'] ) ) {
-        $product_name .= sprintf(
-            '<ul><li>%s: %s</li></ul>',
-                __( 'Your name', 'plugin_republic' ),
-                esc_html( $item['pr_field'] )
-            );
-    }
-    return $product_name;
-   }
-add_filter( 'woocommerce_order_item_name', 'plugin_republic_order_item_name', 10, 2 );
+// function plugin_republic_order_item_name( $product_name, $item ) {
+//     if( isset( $item['pr_field'] ) ) {
+//         $product_name .= sprintf(
+//             '<ul><li>%s: %s</li></ul>',
+//                 __( 'Your name', 'plugin_republic' ),
+//                 esc_html( $item['pr_field'] )
+//             );
+//     }
+//     return $product_name;
+//    }
+// add_filter( 'woocommerce_order_item_name', 'plugin_republic_order_item_name', 10, 2 );
 
 
 /**
@@ -280,22 +280,54 @@ function custom_checkout_field($checkout){
 
 	global $woocommerce;
 	$items = $woocommerce->cart->get_cart();
-	
-	
-	
 
-	echo '<div id="custom_checkout_field"><h4>' . __('Boleto') . '</h4>';
-	woocommerce_form_field('custom_field_name', array(
-		'type' => 'text',
-		'class' => array(
-			'my-field-class form-row-wide'
-		),
-		'label' => __('Custom Additional Field') ,
-		'placeholder' => __('New Custom Field') ,
-	),
-	$checkout->get_value('custom_field_name'));
-	echo '</div>';
+	foreach ($items as $key => $value) {
+		$boletos = $value['boleto'];
+	}
 
+	foreach ($boletos as $boleto) {
+
+		echo '<div id="custom_checkout_field"><h4>' . __('Boleto') . " " . $boleto . '</h4>';
+
+			woocommerce_form_field('custom_field_name', array(
+				'type' => 'text',
+				'class' => array(
+					'my-field-class form-row-wide'
+				),
+				'label' => __('Nombre')
+			),
+			$checkout->get_value('custom_field_name'));
+
+			woocommerce_form_field('custom_field_name', array(
+				'type' => 'text',
+				'class' => array(
+					'my-field-class form-row-wide'
+				),
+				'label' => __('Apellido')
+			),
+			$checkout->get_value('custom_field_name'));
+
+			woocommerce_form_field('custom_field_name', array(
+				'type' => 'text',
+				'class' => array(
+					'my-field-class form-row-wide'
+				),
+				'label' => __('Teléfono')
+			),
+			$checkout->get_value('custom_field_name'));
+
+			woocommerce_form_field('custom_field_name', array(
+				'type' => 'text',
+				'class' => array(
+					'my-field-class form-row-wide'
+				),
+				'label' => __('Correo')
+			),
+			$checkout->get_value('custom_field_name'));
+
+		echo '</div>';
+
+	}
 
 }
 
@@ -304,8 +336,10 @@ function custom_checkout_field($checkout){
 */
 add_action('woocommerce_checkout_update_order_meta', 'custom_checkout_field_update_order_meta');
 function custom_checkout_field_update_order_meta($order_id){
+
 	if (!empty($_POST['custom_field_name'])) {
 		update_post_meta($order_id, 'Some Field', sanitize_text_field($_POST['custom_field_name']));
 	}
+	
 }
 add_action( 'woocommerce_add_order_item_meta', 'tshirt_order_meta_handler', 1, 3 );
