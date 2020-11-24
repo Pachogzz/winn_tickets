3656f6c55821b2876665a6bb30c38ed61238b97a
Pacho Gonzalez <pachogzz@gmail.com>
2020-11-24 09:34:20 -0600

    Se ocultaron los campos de shipping y billing

--- inc/winn-event-mods.php
+++ inc/winn-event-mods.php
@@ -4,9 +4,9 @@
  */
 function my_remove_product_type_options( $options ) {
 	// Virtual product checkbox
-	if ( isset( $options['virtual'] ) ) {
-		unset( $options['virtual'] );
-	}
+	// if ( isset( $options['virtual'] ) ) {
+	// 	unset( $options['virtual'] );
+	// }
     
 	// Downloable product checkbox
 	if ( isset( $options['downloadable'] ) ) {
@@ -128,3 +128,41 @@ function custom_css_fix() {
     }
   </style>';
 }
+
+ /**
+ Remove all possible fields
+ **/
+function wc_remove_checkout_fields( $fields ) {
+
+    // Billing fields
+    // unset( $fields['billing']['billing_first_name'] );
+    // unset( $fields['billing']['billing_last_name'] );
+    // unset( $fields['billing']['billing_email'] );
+    // unset( $fields['billing']['billing_phone'] );
+    unset( $fields['billing']['billing_company'] );
+    unset( $fields['billing']['billing_address_1'] );
+    unset( $fields['billing']['billing_address_2'] );
+    unset( $fields['billing']['billing_city'] );
+    unset( $fields['billing']['billing_state'] );
+    unset( $fields['billing']['billing_country'] );
+    unset( $fields['billing']['billing_postcode'] );
+
+    // Shipping fields
+    // unset( $fields['shipping']['shipping_first_name'] );
+    // unset( $fields['shipping']['shipping_last_name'] );
+    // unset( $fields['shipping']['shipping_email'] );
+    // unset( $fields['shipping']['shipping_phone'] );
+    unset( $fields['shipping']['shipping_company'] );
+    unset( $fields['shipping']['shipping_address_1'] );
+    unset( $fields['shipping']['shipping_address_2'] );
+    unset( $fields['shipping']['shipping_city'] );
+    unset( $fields['shipping']['shipping_state'] );
+    unset( $fields['shipping']['shipping_country'] );
+    unset( $fields['shipping']['shipping_postcode'] );
+
+    // Order fields
+    unset( $fields['order']['order_comments'] );
+
+    return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'wc_remove_checkout_fields' );
