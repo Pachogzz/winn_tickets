f88ae83fa17007b4206cace9ec4cb51c7aa55d12
Andrés Venegas <andresvenegas@MacBook-Pro.local>
2020-12-08 12:18:00 -0600

    Se agrego en el mail los boletos comprados

--- inc/boletos.php
+++ inc/boletos.php
@@ -462,3 +462,15 @@ function custom_checkout_field_update_order_meta($order_id){
 	
 }
 // add_action( 'woocommerce_add_order_item_meta', 'tshirt_order_meta_handler', 1, 3 );
+
+
+// Mail boletos
+add_filter( 'woocommerce_email_order_meta_fields', 'custom_woocommerce_email_order_meta_fields', 10, 3 );
+
+function custom_woocommerce_email_order_meta_fields( $fields, $sent_to_admin, $order ) {
+    $fields['meta_key'] = array(
+        'label' => __( 'Boletos comprados: ' ),
+        'value' => get_post_meta( $order->id, 'boletos_comprados', true ),
+    );
+    return $fields;
+}
